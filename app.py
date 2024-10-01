1a9e832e7e30161ef60159a40d4ba47d2a48e224
Thg-Projects <thgkamikaz@gmail.com>
2024-10-01 20:39:05 -0300

    v13

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def index():
 if __name__ == '__main__':
     # app.register_blueprint(routes_app)  # Registra as rotas do módulo routes
     app.register_blueprint(routes)
-
-    app.run(host="127.0.0.1", port="2020",debug=True)
+    app.run(debug=True)
+    # app.run(host="127.0.0.1", port="2020",debug=True)
     # http://127.0.0.1:5000/
     # app.run(host="127.0.0.1", port="2020")
 
